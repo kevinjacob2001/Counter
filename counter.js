d3d5efef8dd89a6f48871ebe928ca24de5611847
Kevin Jacob <kevinjacob2001@gmail.com>
2020-06-22 14:29:00 +0530

    removed the issue that caused negative counting

--- counter.js
+++ counter.js
@@ -1,4 +1,4 @@
-alert("Hello there! Welcome to Timer.kj. Here you can increment and decrement the digit to a particular value and initiate a count-down where value gets decremented by one every 0.5 seconds until the counter reaches 0. User can reset the value to an initial value which is set as 25.")
+//alert("Hello there! Welcome to Timer.kj. Here you can increment and decrement the digit to a particular value and initiate a count-down where value gets decremented by one every 0.5 seconds until the counter reaches 0. User can reset the value to an initial value which is set as 25.")
 
 let val = document.getElementById("field").innerHTML;
 
@@ -14,20 +14,25 @@ document.querySelector("#Decrement").addEventListener("click", function () {
 })
 
 document.querySelector("#reset").addEventListener("click", function () {
-    val = 0;
+    val = 25;
     document.querySelector("#field").textContent = val;
 })
 
 document.querySelector("#start").addEventListener("click", function () {
-
+if(val>-1){
     var x = setInterval(function () {
+       
         document.querySelector("#field").textContent = val;
-        val--;
+        --val;
         if (val == -2) {
             alert("The timer has reached zero!")
             clearInterval(x);
-        }
+      
+    }
+     
+
     }, 500)
+}
 }
 );
 
